d1a27c5be62e9ffa7e861d8a77f6cecf5f471303
Wilmer Vasquez <57019324+WilmerVasquez@users.noreply.github.com>
2021-02-05 21:51:31 -0600

    Add files via upload

---
+++ Tarea 1 LFP 201800678/nest list.py
@@ -0,0 +1,12 @@
+if name == 'main':
+    students = []
+    for _ in range(int(input())):
+        name = input()
+        score = float(input())
+
+
+
+        students.append([name,score])
+
+    second_high = sorted(set([i[1] for i in students]))[1]
+    print("\n".join(sorted([i[0] for i in students if i[1] == second_high])))
---
+++ Tarea 1 LFP 201800678/tuples.py
@@ -0,0 +1,4 @@
+if __name__ == '__main__':
+    n = int(input())
+
+    print(hash(tuple(int(x) for x in input().split())))
